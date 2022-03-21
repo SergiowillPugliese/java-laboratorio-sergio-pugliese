85a24acf6e63a5ab03c8119ccae4424f1a7d67ce
Sergio Pugliese <pugliese.sergio1987@hotmail.it>
2022-03-21 11:06:45 +0100

    aggiornamento

--- JavaInterfacciaGrafica/src/main/java/com/engim/javainterfacciagrafica/HelloApplication.java
+++ JavaInterfacciaGrafica/src/main/java/com/engim/javainterfacciagrafica/HelloApplication.java
@@ -1,5 +1,6 @@
 package com.engim.javainterfacciagrafica;
 
+import com.engim.javainterfacciagrafica.model.Registro;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
